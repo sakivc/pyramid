999c864225c499294c5710e2251ff07074209f9e
vikas <34834691+sakivc@users.noreply.github.com>
2024-09-24 12:09:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def app1():
         pattern = left_pyramid(n)
         return render_template("/app1/result.html",paragraph=pattern)
 
-@app.rout("/right")
+@app.route("/right")
 def app2():
     if request.method == "GET":
         return render_template("/app2/index.html")
